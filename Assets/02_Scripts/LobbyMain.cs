d471c9426d92f8efe0c4b3303e738580af139009
JiHwanNo <83812895+JiHwanNo@users.noreply.github.com>
2026-02-19 13:46:37 +0900

    에셋 정리 및 씬 업데이트

--- Assets/02_Scripts/LobbyMain.cs
+++ Assets/02_Scripts/LobbyMain.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class LobbyMain : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        var popupManager = PopupManager.Instance;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
